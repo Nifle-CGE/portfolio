35c3539abee4b37eab49583ea63c95418a38917c
Nifle-CGE <pub@elfin.fr>
2026-01-25 10:50:31 +0100

    start making the career page

--- src/routes/career/+page.svelte
+++ src/routes/career/+page.svelte
@@ -1,7 +1,107 @@
-<h2 class="text-xl">Parcours</h2>
+<div class="flex flex-col items-center gap-6">
+	<h2 class="font-grotesk text-3xl font-semibold">Mon Parcours</h2>
 
-<p>Cote a cote : BUT3 | alternance Oktal</p>
-<p>stage oktal</p>
-<p>BUT2</p>
-<p>Prepa t2 1A</p>
-<p>Bac Math Info mention tres bien</p>
+	<ul class="timeline timeline-vertical">
+		<li>
+			<div class="timeline-start">Septembre 2025 - Aujourd'hui</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end flex flex-col gap-2 lg:flex-row">
+				<div class="timeline-box">alternance Oktal</div>
+				<div class="timeline-box">BUT3</div>
+			</div>
+			<hr />
+		</li>
+		<li>
+			<hr />
+			<div class="timeline-start">Avril 2025 - Juillet 2025</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end timeline-box">stage oktal</div>
+			<hr />
+		</li>
+		<li>
+			<hr />
+			<div class="timeline-start">Septembre 2024 - Mars 2025</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end timeline-box">BUT2</div>
+			<hr />
+		</li>
+		<li>
+			<hr />
+			<div class="timeline-start">Septembre 2023 - Juin 2024</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end timeline-box">Prepa t2 1A</div>
+			<hr />
+		</li>
+		<li>
+			<hr />
+			<div class="timeline-start">Septembre 2020 - Juin 2023</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end timeline-box">Bac Math Info mention tres bien</div>
+		</li>
+	</ul>
+</div>
