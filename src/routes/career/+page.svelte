dd8333053977f8528fe438adb1fbc12d3613effd
Nifle-CGE <pub@elfin.fr>
2026-01-25 18:26:06 +0100

    finish the career page

--- src/routes/career/+page.svelte
+++ src/routes/career/+page.svelte
@@ -1,9 +1,9 @@
 <div class="flex flex-col items-center gap-6">
 	<h2 class="font-grotesk text-3xl font-semibold">Mon Parcours</h2>
 
-	<ul class="timeline timeline-vertical">
+	<ul class="timeline timeline-snap-icon lg:timeline-vertical">
 		<li>
-			<div class="timeline-start">Septembre 2025 - Aujourd'hui</div>
+			<div class="timeline-start lg:self-start">Septembre 2025 - Aujourd'hui</div>
 			<div class="timeline-middle">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -18,15 +18,61 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end flex flex-col gap-2 lg:flex-row">
-				<div class="timeline-box">alternance Oktal</div>
-				<div class="timeline-box">BUT3</div>
+			<div class="timeline-end flex flex-col gap-2 font-inter text-sm text-neutral-content">
+				<div class="flex flex-col gap-2 timeline-box bg-base-200">
+					<p class="text-lg text-primary-content">Alternance</p>
+					<p class="text-base text-secondary-content">
+						a <a class="link" href="https://sogeclair.com/simulation/simulation-ferroviaire"
+							>Oktal</a
+						>, filiale de <a class="link" href="https://sogeclair.com">Sogeclair</a>, Blagnac
+					</p>
+
+					<ul class="list bg-base-100">
+						<li class="list-row p-3">
+							Taches de recherche et developpement pour l'orchestration de tests automatises
+						</li>
+						<li class="list-row p-3">
+							Prise en compte du feedback des utilisateurs pour un outil interne
+						</li>
+						<li class="list-row p-3">
+							Utilisation reguliere de l'anglais technique avec les equipes internationales
+						</li>
+					</ul>
+
+					<div class="font-semibold">
+						<span class="badge px-2 badge-md badge-accent">Python</span>
+						<span class="badge px-2 badge-md badge-accent">Metaprogramming</span>
+						<span class="badge px-2 badge-md badge-accent">Design d'UI</span>
+						<span class="badge px-2 badge-md badge-accent">WebSockets</span>
+					</div>
+				</div>
+				<div class="flex flex-col gap-2 timeline-box bg-base-200">
+					<p class="text-lg text-primary-content">Troisieme annee de BUT Informatique</p>
+					<p class="text-base text-secondary-content">
+						a l'<a class="link" href="https://iut.univ-tlse3.fr/bachelor-specialite-informatique"
+							>IUT Paul Sabatier</a
+						>, Toulouse
+					</p>
+
+					<ul class="list bg-base-100">
+						<li class="list-row p-3">
+							Specialite réalisation d'applications : conception, développement, validation
+						</li>
+					</ul>
+
+					<div class="font-semibold">
+						<span class="badge px-2 badge-md badge-accent">Java</span>
+						<span class="badge px-2 badge-md badge-accent">Python</span>
+						<span class="badge px-2 badge-md badge-accent">MongoDB</span>
+						<span class="badge px-2 badge-md badge-accent">neo4j</span>
+					</div>
+				</div>
 			</div>
 			<hr />
 		</li>
 		<li>
 			<hr />
-			<div class="timeline-start">Avril 2025 - Juillet 2025</div>
+			<div class="timeline-start lg:self-start">Avril 2025 - Juillet 2025</div>
 			<div class="timeline-middle">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -41,12 +87,41 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end timeline-box">stage oktal</div>
+			<div
+				class="timeline-end flex flex-col gap-2 timeline-box bg-base-200 font-inter text-sm text-neutral-content"
+			>
+				<p class="text-lg text-primary-content">Stage de fin d'annee de BUT2</p>
+				<p class="text-base text-secondary-content">
+					a <a class="link" href="https://sogeclair.com/simulation/simulation-ferroviaire">Oktal</a
+					>, filiale de <a class="link" href="https://sogeclair.com">Sogeclair</a>, Blagnac
+				</p>
+
+				<ul class="list bg-base-100">
+					<li class="list-row p-3">
+						Apprentissage de la gestion de documentation en entreprise (Confluence, Jira)
+					</li>
+					<li class="list-row p-3">
+						Elaboration de scenarios de tests automatises pour des simulateurs ferroviaires
+					</li>
+					<li class="list-row p-3">
+						Application de techniques de gestion de projet (Agile, Scrum)
+					</li>
+				</ul>
+
+				<div class="font-semibold">
+					<span class="badge px-2 badge-md badge-accent">Python</span>
+					<span class="badge px-2 badge-md badge-accent">Pytest</span>
+					<span class="badge px-2 badge-md badge-accent">Optimisation</span>
+					<span class="badge px-2 badge-md badge-accent">BaseX</span>
+					<span class="badge px-2 badge-md badge-accent">Profilage</span>
+					<span class="badge px-2 badge-md badge-accent">SVN</span>
+				</div>
+			</div>
 			<hr />
 		</li>
 		<li>
 			<hr />
-			<div class="timeline-start">Septembre 2024 - Mars 2025</div>
+			<div class="timeline-start lg:self-start">2024 - Mars 2025</div>
 			<div class="timeline-middle">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -61,12 +136,45 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end timeline-box">BUT2</div>
+			<div
+				class="timeline-end flex flex-col gap-2 timeline-box bg-base-200 font-inter text-sm text-neutral-content"
+			>
+				<p class="text-lg text-primary-content">Deuxieme annee de BUT Informatique</p>
+				<p class="text-base text-secondary-content">
+					a l'<a class="link" href="https://iut.univ-tlse3.fr/bachelor-specialite-informatique"
+						>IUT Paul Sabatier</a
+					>, Toulouse
+				</p>
+
+				<ul class="list bg-base-100">
+					<li class="list-row p-3">
+						Specialite réalisation d'applications : conception, développement, validation
+					</li>
+					<li class="list-row p-3">
+						<p>
+							Beaucoup de <a class="link" href="/projects">projets</a> avec des themes divers et varies
+						</p>
+					</li>
+					<li class="list-row p-3">
+						Apprentissage de techniques de gestion de projet (Agile, Scrum)
+					</li>
+				</ul>
+
+				<div class="font-semibold">
+					<span class="badge px-2 badge-md badge-accent">Java</span>
+					<span class="badge px-2 badge-md badge-accent">Web</span>
+					<span class="badge px-2 badge-md badge-accent">SQL</span>
+					<span class="badge px-2 badge-md badge-accent">PHP</span>
+					<span class="badge px-2 badge-md badge-accent">Docker</span>
+					<span class="badge px-2 badge-md badge-accent">Vagrant</span>
+					<span class="badge px-2 badge-md badge-accent">Flutter</span>
+				</div>
+			</div>
 			<hr />
 		</li>
 		<li>
 			<hr />
-			<div class="timeline-start">Septembre 2023 - Juin 2024</div>
+			<div class="timeline-start lg:self-start">2023 - 2024</div>
 			<div class="timeline-middle">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -81,12 +189,37 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end timeline-box">Prepa t2 1A</div>
+			<div
+				class="timeline-end flex flex-col gap-2 timeline-box bg-base-200 font-inter text-sm text-neutral-content"
+			>
+				<p class="text-lg text-primary-content">Premiere annee de CPGE MPI</p>
+				<p class="text-base text-secondary-content">
+					a la <a
+						class="link"
+						href="https://www.inp-toulouse.fr/fr/formations/la-prepa-des-inp.html"
+						>Prepa t2 (anciennement Prepa des INP)</a
+					>, Toulouse
+				</p>
+
+				<ul class="list bg-base-100">
+					<li class="list-row p-3">Surtout des projets informatiques liés aux mathematiques</li>
+					<li class="list-row p-3">
+						C'est pendant cette annee que je me rends compte que je veux me concentrer sur
+						l'informatique
+					</li>
+				</ul>
+
+				<div class="font-semibold">
+					<span class="badge px-2 badge-md badge-accent">Data science</span>
+					<span class="badge px-2 badge-md badge-accent">Numpy</span>
+					<span class="badge px-2 badge-md badge-accent">Matplotlib</span>
+				</div>
+			</div>
 			<hr />
 		</li>
 		<li>
 			<hr />
-			<div class="timeline-start">Septembre 2020 - Juin 2023</div>
+			<div class="timeline-start lg:self-start">2020 - 2023</div>
 			<div class="timeline-middle">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -101,7 +234,25 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end timeline-box">Bac Math Info mention tres bien</div>
+			<div
+				class="timeline-end flex flex-col gap-2 timeline-box bg-base-200 font-inter text-sm text-neutral-content"
+			>
+				<p class="text-lg text-primary-content">Baccalauréat europeen mention Tres Bien</p>
+				<p class="text-base text-secondary-content">au Lycee Saint Joseph - La Salle Toulouse</p>
+
+				<ul class="list bg-base-100">
+					<li class="list-row p-3">Spécialités Mathematiques, Informatique et Physique Chimie</li>
+					<li class="list-row p-3">Debut de ma passion pour l'informatique</li>
+					<li class="list-row p-3">
+						<p>Je realise le projet personnel <a class="link" href="/projects#rbs">RBS</a></p>
+					</li>
+				</ul>
+
+				<div class="font-semibold">
+					<span class="badge px-2 badge-md badge-accent">Python</span>
+					<span class="badge px-2 badge-md badge-accent">Web</span>
+				</div>
+			</div>
 		</li>
 	</ul>
 </div>
