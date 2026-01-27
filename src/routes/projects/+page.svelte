911bf852198a95d529e79a74ed59289d03ddf6d1
Nifle-CGE <pub@elfin.fr>
2026-01-27 23:32:54 +0100

    finish projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import gestion_immo_img from '$lib/assets/img/projects/gestion_immo.png';
+	import gestion_sport_img from '$lib/assets/img/projects/hawkey.png';
 </script>
 
 <div class="flex flex-col items-center gap-6 font-inter">
@@ -10,7 +11,7 @@
 			<summary id="rbs" class="collapse-title text-lg font-semibold text-primary-content">
 				RandomBroadcastingSelector
 			</summary>
-			<div class="collapse-content space-y-3">
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
 				<div class="flex flex-col lg:flex-row">
 					<div>
 						<h4 class="text-sm text-discrete-content">Description</h4>
@@ -26,9 +27,13 @@
 					<div>
 						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
 						<div class="flex flex-col gap-2">
-							<a class="badge link badge-lg badge-secondary" href="/skills#realiser">Realiser</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#optimiser">Optimiser</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#gerer">Gerer</a>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#realiser"
+								>Realiser</a
+							>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#optimiser"
+								>Optimiser</a
+							>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#gerer">Gerer</a>
 						</div>
 					</div>
 				</div>
@@ -41,8 +46,12 @@
 						<span class="badge px-2 badge-md badge-accent">Docker</span>
 					</div>
 					<div>
-						<a class="link" href="https://web-rbs.com/">Site web &nearrow;</a>
-						<a class="link" href="https://github.com/Nifle-CGE/RandomBroadcastingSelector">
+						<a class="link" href="https://web-rbs.com/" target="_blank">Site web &nearrow;</a>
+						<a
+							class="link"
+							href="https://github.com/Nifle-CGE/RandomBroadcastingSelector"
+							target="_blank"
+						>
 							Sur GitHub &nearrow;
 						</a>
 					</div>
@@ -51,10 +60,10 @@
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content">
+			<summary id="gestion-immo" class="collapse-title text-lg font-semibold text-primary-content">
 				Application de gestion immobiliere
 			</summary>
-			<div class="collapse-content space-y-3">
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
 				<div class="flex flex-col lg:flex-row">
 					<div>
 						<h4 class="text-sm text-discrete-content">Description</h4>
@@ -62,8 +71,8 @@
 							Une application de gestion de biens immobiliers réalisée en groupe de 4 dans le cadre
 							du BUT.
 							<br />
-							Cette application permet de gérer des biens, des locataires, des contrats de location, et
-							des travaux.
+							Cette application permet de gérer des des biens immobiliers, des locataires, des baux et
+							des travaux associés
 						</p>
 						<button
 							class="group flex h-25 w-30 cursor-pointer items-center justify-center rounded-md bg-cover bg-center"
@@ -88,7 +97,7 @@
 							</svg>
 						</button>
 						<dialog id="img_modal_1" class="modal">
-							<div class="modal-box">
+							<div class="modal-box w-auto max-w-5xl">
 								<form method="dialog">
 									<button class="btn absolute top-0 right-0 btn-circle btn-ghost btn-sm">✕</button>
 								</form>
@@ -103,10 +112,14 @@
 					<div>
 						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
 						<div class="flex flex-col gap-2">
-							<a class="badge link badge-lg badge-secondary" href="/skills#realiser">Realiser</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#gerer">Gerer</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#conduire">Conduire</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#collaborer">
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#realiser"
+								>Realiser</a
+							>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#gerer">Gerer</a>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#conduire"
+								>Conduire</a
+							>
+							<a class="badge link px-2 badge-lg badge-secondary" href="/skills#collaborer">
 								Collaborer
 							</a>
 						</div>
@@ -115,24 +128,70 @@
 				<div class="flex flex-row justify-between">
 					<div>
 						<span class="badge px-2 badge-md badge-accent">Java</span>
-						<span class="badge px-2 badge-md badge-accent">A REMPLIR</span>
+						<span class="badge px-2 badge-md badge-accent">MVC</span>
+						<span class="badge px-2 badge-md badge-accent">SQLite</span>
+						<span class="badge px-2 badge-md badge-accent">Swing</span>
+						<span class="badge px-2 badge-md badge-accent">JUnit</span>
+						<span class="badge px-2 badge-md badge-accent">GUI</span>
 					</div>
 				</div>
 			</div>
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content">
+			<summary id="portfolio" class="collapse-title text-lg font-semibold text-primary-content">
 				Ce portfolio
 			</summary>
-			<div class="collapse-content text-sm">Placeholder</div>
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
+				<div class="flex flex-col lg:flex-row">
+					<div>
+						<h4 class="text-sm text-discrete-content">Description</h4>
+						<p>
+							Decouverte de SvelteKit et TailwindCSS pour ce projet suite a la recommandation d'un <a
+								class="link"
+								href="https://portfolio-s6.paillaugue.fr/"
+								target="_blank">ami</a
+							>.
+							<br />
+							Utilisation de l'IA tres limitee, seulement pour me debloquer notamment sur l'utilisation
+							de "flex" en CSS.
+						</p>
+					</div>
+					<div class="divider lg:divider-horizontal"></div>
+					<div>
+						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
+						<div class="flex flex-col gap-2">
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#realiser">Realiser</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#conduire">Conduire</a>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-row justify-between">
+					<div>
+						<span class="badge px-2 badge-md badge-accent">SvelteKit</span>
+						<span class="badge px-2 badge-md badge-accent">Web</span>
+						<span class="badge px-2 badge-md badge-accent">Responsive</span>
+						<span class="badge px-2 badge-md badge-accent">TailwindCSS</span>
+						<span class="badge px-2 badge-md badge-accent">daisyUI</span>
+					</div>
+					<div>
+						<a class="link" href="https://elfin.fr/" target="_blank">Site web &nearrow;</a>
+						<a class="link" href="https://github.com/Nifle-CGE/portfolio" target="_blank">
+							Sur GitHub &nearrow;
+						</a>
+					</div>
+				</div>
+			</div>
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content">
-				R5.A12 Algorithme genetique
+			<summary
+				id="algo-genetique"
+				class="collapse-title text-lg font-semibold text-primary-content"
+			>
+				Algorithme genetique
 			</summary>
-			<div class="collapse-content space-y-3">
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
 				<div class="flex flex-col lg:flex-row">
 					<div>
 						<h4 class="text-sm text-discrete-content">Description</h4>
@@ -143,6 +202,7 @@
 							<a
 								class="link"
 								href="https://fr.wikipedia.org/wiki/Probl%C3%A8me_du_voyageur_de_commerce"
+								target="_blank"
 							>
 								voyageur de commerce
 							</a>.
@@ -154,8 +214,8 @@
 					<div>
 						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
 						<div class="flex flex-col gap-2">
-							<a class="badge link badge-lg badge-secondary" href="/skills#optimiser">Optimiser</a>
-							<a class="badge link badge-lg badge-secondary" href="/skills#collaborer">
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#optimiser">Optimiser</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#collaborer">
 								Collaborer
 							</a>
 						</div>
@@ -169,7 +229,11 @@
 						<span class="badge px-2 badge-md badge-accent">TUI</span>
 					</div>
 					<div>
-						<a class="link" href="https://github.com/Nifle-CGE/R5.12-Algo-genetiques">
+						<a
+							class="link"
+							href="https://github.com/Nifle-CGE/R5.12-Algo-genetiques"
+							target="_blank"
+						>
 							Sur GitHub &nearrow;
 						</a>
 					</div>
@@ -178,22 +242,195 @@
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content">
-				Application de gestion d'equipe de sport
+			<summary id="gestion_sport" class="collapse-title text-lg font-semibold text-primary-content">
+				Site de gestion d'equipe de sport
 			</summary>
-			<div class="collapse-content text-sm">Placeholder</div>
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
+				<div class="flex flex-col lg:flex-row">
+					<div>
+						<h4 class="text-sm text-discrete-content">Description</h4>
+						<p>
+							Une application web de gestion d'equipe de sport réalisée en binôme dans le cadre du
+							BUT.
+							<br />
+							Ce site permet de gérer des joueurs, des matchs et des statistiques.
+						</p>
+						<button
+							class="group flex h-25 w-30 cursor-pointer items-center justify-center rounded-md bg-cover bg-center"
+							onclick={img_modal_2.showModal()}
+							style="background-image: url({gestion_sport_img});"
+							aria-label="Aperçu de l'application de gestion immobiliere"
+							title="Aperçu de l'application de gestion immobiliere"
+						>
+							<svg
+								class="h-16 w-16 text-transparent transition-colors group-hover:text-white"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M15 9L21 3M21 3H15M21 3V9M9 9L3 3M3 3L3 9M3 3L9 3M9 15L3 21M3 21H9M3 21L3 15M15 15L21 21M21 21V15M21 21H15"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</button>
+						<dialog id="img_modal_2" class="modal">
+							<div class="modal-box w-auto max-w-5xl">
+								<form method="dialog">
+									<button class="btn absolute top-0 right-0 btn-circle btn-ghost btn-sm">✕</button>
+								</form>
+								<img src={gestion_sport_img} alt="Aperçu de l'application de gestion immobiliere" />
+							</div>
+							<form method="dialog" class="modal-backdrop">
+								<button>close</button>
+							</form>
+						</dialog>
+					</div>
+					<div class="divider lg:divider-horizontal"></div>
+					<div>
+						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
+						<div class="flex flex-col gap-2">
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#realiser">Realiser</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#gerer">Gerer</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#conduire">Conduire</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#collaborer">
+								Collaborer
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-row justify-between">
+					<div>
+						<span class="badge px-2 badge-md badge-accent">PHP</span>
+						<span class="badge px-2 badge-md badge-accent">Web</span>
+						<span class="badge px-2 badge-md badge-accent">SQL</span>
+						<span class="badge px-2 badge-md badge-accent">CI/CD</span>
+						<span class="badge px-2 badge-md badge-accent">API REST</span>
+						<span class="badge px-2 badge-md badge-accent">Authentification</span>
+					</div>
+				</div>
+			</div>
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content">
-				Chat en ligne
+			<summary id="loottracker" class="collapse-title text-lg font-semibold text-primary-content">
+				Application mobile journal de trading
 			</summary>
-			<div class="collapse-content text-sm">Placeholder</div>
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
+				<div class="flex flex-col lg:flex-row">
+					<div>
+						<h4 class="text-sm text-discrete-content">Description</h4>
+						<p>
+							Un journal de trading réalisée en binôme dans le cadre du BUT.
+							<br />
+							Elle permet aux traders de suivre leurs performances et d'analyser leurs résultats.
+						</p>
+					</div>
+					<div class="divider lg:divider-horizontal"></div>
+					<div>
+						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
+						<div class="flex flex-col gap-2">
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#realiser">Realiser</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#conduire">Conduire</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#collaborer">
+								Collaborer
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-row justify-between">
+					<div>
+						<span class="badge px-2 badge-md badge-accent">Flutter</span>
+						<span class="badge px-2 badge-md badge-accent">UX</span>
+						<span class="badge px-2 badge-md badge-accent">Dart</span>
+						<span class="badge px-2 badge-md badge-accent">Gradle</span>
+						<span class="badge px-2 badge-md badge-accent">SQL</span>
+					</div>
+				</div>
+			</div>
 		</details>
 
 		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
-			<summary class="collapse-title text-lg font-semibold text-primary-content"> Ze Zone </summary>
-			<div class="collapse-content text-sm">Placeholder</div>
+			<summary id="bombochat" class="collapse-title text-lg font-semibold text-primary-content">
+				Bombochat
+			</summary>
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
+				<div class="flex flex-col lg:flex-row">
+					<div>
+						<h4 class="text-sm text-discrete-content">Description</h4>
+						<p>
+							Un projet de chat en ligne réalisé en binôme dans le cadre du BUT.
+							<br />
+							Les utilisateurs peuvent choisir un pseudo et discuter en temps réel avec les autres utilisateurs
+							connectés.
+							<br />
+							Nous vous invitons à y laisser un message pour les futurs visiteurs !
+						</p>
+					</div>
+					<div class="divider lg:divider-horizontal"></div>
+					<div>
+						<h4 class="pb-2 text-sm text-discrete-content">Competences associees</h4>
+						<div class="flex flex-col gap-2">
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#realiser">Realiser</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#administrer"
+								>Administrer</a
+							>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#conduire">Conduire</a>
+							<a class="badge link badge-lg badge-secondary px-2" href="/skills#collaborer">
+								Collaborer
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-row justify-between">
+					<div>
+						<span class="badge px-2 badge-md badge-accent">Web</span>
+						<span class="badge px-2 badge-md badge-accent">Ajax</span>
+						<span class="badge px-2 badge-md badge-accent">JQuery</span>
+						<span class="badge px-2 badge-md badge-accent">MySQL</span>
+						<span class="badge px-2 badge-md badge-accent">CI/CD</span>
+					</div>
+					<div>
+						<a class="link" href="https://bombochat.infy.uk/" target="_blank">Site web &nearrow;</a>
+					</div>
+				</div>
+			</div>
+		</details>
+
+		<details class="collapse-arrow collapse border border-base-300 bg-base-200">
+			<summary id="ze-zone" class="collapse-title text-lg font-semibold text-primary-content">
+				Ze Zone
+			</summary>
+			<div class="collapse-content space-y-3 border-t-2 border-base-content/10 pt-2">
+				<div>
+					<h4 class="text-sm text-discrete-content">Description</h4>
+					<p>
+						Pas du tout un site web ou une application, mais un jeu de societe cree entierement par
+						moi.
+						<br />
+						Ze Zone est un jeu de plateau dans lequel les joueurs s'affrontent dans un milieu qui devient
+						de plus en plus difficile a naviguer au fur et a mesure que la partie avance.
+						<br />
+						En vente en ligne sur le site ci-dessous ou en personne lors d'evenements.
+					</p>
+				</div>
+				<div class="flex flex-row justify-between">
+					<div>
+						<span class="badge px-2 badge-md badge-accent">Onshape</span>
+						<span class="badge px-2 badge-md badge-accent">Impression 3D</span>
+						<span class="badge px-2 badge-md badge-accent">Vente en personne</span>
+						<span class="badge px-2 badge-md badge-accent">Explication de regles</span>
+					</div>
+					<div>
+						<a class="link" href="https://games.a-zuga.com/ze-zone" target="_blank">
+							Site web &nearrow;
+						</a>
+					</div>
+				</div>
+			</div>
 		</details>
 	</div>
 </div>
