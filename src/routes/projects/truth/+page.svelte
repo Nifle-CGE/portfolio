86977820d7b448623bc53d92787b2dc35a2cfb5d
Nifle-CGE <elfin@elfin.fr>
2026-04-04 13:56:14 +0200

    update dependencies

--- src/routes/projects/truth/+page.svelte
+++ src/routes/projects/truth/+page.svelte
@@ -1,10 +1,23 @@
+<script lang="ts">
+	import pareidolia from '$lib/assets/img/pareidolia_masonry.webp';
+</script>
+
 <svelte:head>
 	<title>Truth</title>
 </svelte:head>
 
 <div style="all: initial;">
 	<div class="bg-white">
-		<h1 class="text-3xl">Truth</h1>
-		<p>WIP</p>
+		<h1 class="text-3xl">Human - Human</h1>
+		<img src={pareidolia} alt="Examples of Pareidolia" />
+		<p>
+			<a
+				href="https://grapheine.com/en/magazine/pareidolia-art-of-seeing-faces-everywhere/"
+				class="link text-blue-900"
+				target="_blank"
+			>
+				Source
+			</a>
+		</p>
 	</div>
 </div>
